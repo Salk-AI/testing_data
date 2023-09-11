6a8ea49b0e1683f67cfaadf05a8be186d6e0871b
Prathmesh Tokekar <patpat8@users.noreply.github.com>
2023-09-11 10:09:06 +0530

    Faiss index from pdf and text

---
+++ create_faiss.py
@@ -0,0 +1,85 @@
+from PyPDF2 import PdfReader 
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.vectorstores import FAISS 
+#from langchain.chains.question_answering import load_qa_chain
+from langchain.document_loaders import TextLoader , CSVLoader 
+from langchain.llms import OpenAI
+from dotenv import load_dotenv
+import openai
+import os
+
+load_dotenv()
+openai.api_key = os.environ['OPENAI_API_KEY']
+
+
+
+#For loading csv files 
+#csv_loader = CSVLoader(file_path="./docs.csv")
+
+
+def pdf_to_faiss(file_path):
+    # Load PDF File from the file path
+    pdf_reader = PdfReader(file_path)
+
+
+    # read data from the file and put them into a variable called raw_text
+    raw_text = ''
+    for i, page in enumerate(pdf_reader.pages):
+        text = page.extract_text()
+        if text:
+            raw_text += text
+
+    # Splitting up the text into smaller chunks for indexing
+    text_splitter = CharacterTextSplitter(        
+        separator = "\n",
+        chunk_size = 1000,
+        chunk_overlap  = 200, #striding over the text
+        length_function = len,
+    )
+    texts = text_splitter.split_text(raw_text)
+
+    # Download embeddings from OpenAI
+    embeddings = OpenAIEmbeddings()
+
+    docsearch = FAISS.from_texts(texts, embeddings)
+    return docsearch
+
+def text_to_faiss(file_path):
+    #Load Text file
+    text_reader = TextLoader(file_path)
+
+    # read data from the file and put them into a variable called raw_text
+    raw_text = ''
+    for i, page in enumerate(text_reader.pages):
+        text = page.extract_text()
+        if text:
+            raw_text += text
+
+    # Splitting up the text into smaller chunks for indexing
+    text_splitter = CharacterTextSplitter(        
+        separator = "\n",
+        chunk_size = 1000,
+        chunk_overlap  = 200, #striding over the text
+        length_function = len,
+    )
+    texts = text_splitter.split_text(raw_text)
+
+    # Download embeddings from OpenAI
+    embeddings = OpenAIEmbeddings()
+
+    docsearch = FAISS.from_texts(texts, embeddings)
+    return docsearch
+
+
+def main():
+    file_path = "fb.pdf"
+    doc_faiss = pdf_to_faiss(file_path)
+    doc_faiss.save_local("fb_1000")
+
+
+
+
+
+if __name__ == "__main__":
+    main()
